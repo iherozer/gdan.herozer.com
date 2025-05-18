f33bd968cec992d3a1657533332c019c9e8badf0
CoderTJP <codertjp@gmail.com>
2025-05-18 14:55:53 -0400

    Updated vite config to import timer route

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
-    "base": "/",
-    "root": 'src',
-    "build": {
-        "outDir": '../dist',
-        "emptyOutDir": true,
+  "base": '/',
+  "root": 'src',
+  "build": {
+    "outDir": '../dist',
+    "emptyOutDir": true,
+    "rollupOptions": {
+      "input": {
+        "main": path.resolve(__dirname, 'src/index.html'),
+        "timer": path.resolve(__dirname, 'src/timer.html'),
+      }
     }
-});
+  }
+})
